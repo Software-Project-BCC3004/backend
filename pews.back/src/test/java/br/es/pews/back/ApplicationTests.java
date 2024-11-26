7ac923140933ab04851eb11b57defd76ad6a6f5e
gabfarmarcondes <gabriel.fmarcondes@hotmail.com>
2024-11-26 15:49:32 -0300

    Adicionando os arquivos e dependências do Spring Boot no projeto #1

---
+++ pews.back/src/test/java/br/es/pews/back/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.es.pews.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
